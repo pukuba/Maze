7b447e35ea30d524412a4daf2975e97ebfe7d9e1
erolf0123 <zaxscd123>
2020-02-26 09:04:06 +0900

    created_maze

--- Maze/main.cpp
+++ Maze/main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 int dx[4]={-1,0,1,0},dy[4]={0,-1,0,1},wall[65][65][4];
 bool check[65][65],pass[4];
+string text_input[65];
 vector<pair<int,int> > connect[65][65];
 bool in(int a,int b){return 1<=a && a<=64 && 1<=b && b<=64;}
 vector<int> create_rand(){
@@ -23,10 +24,7 @@ vector<int> create_rand(){
 */
 void clear(){
     for(int i=1; i<=64; i++){
-        check[i][1] = 1;
-        check[1][i] = 1;
-        check[i][64] = 1;
-        check[64][i] = 1;
+        check[i][1] = check[1][i] = check[i][64] = check[64][i] = 1;
         for(int j=1; j<=64; j++) wall[i][j][0] = wall[i][j][1] = wall[i][j][2] = wall[i][j][3] = 1;
     }
 }
@@ -66,17 +64,47 @@ void draw(){
             if(i == 1 || i == 64){
                 int k = 62;
                 if(i == 1){
-                    cout<<"|_";
-                    k--;
+                    cout<<"|s|";
+                    text_input[i]+="|s|";
+                    k-=2;
+                    while(k--){
+                        cout<<"__";
+                        text_input[i]+="__";
+                    }
+                    text_input[i]+="_|";
+                    cout<<"_|";
+                    break;
                 }
-                while(k--) cout<<"__";
-                if(i == 64) cout<<"_";
-                if(i == 1) cout<<"|";
+                while(k--){
+                    cout<<"__";
+                    text_input[i]+="__";
+                }
+                if(i == 64){
+                    cout<<"_";
+                    text_input[i]+="_";
+                }
+                if(i == 1){
+                    cout<<"|";
+                    text_input[i]+="|";
+                }
                 break;
             }
-            if(wall[i][j][0]) cout<<"| ";
-            else if(wall[i][j][3]) cout<<"__";
-            else cout<<"  ";
+            if(wall[i][j][0]){
+                cout<<"| ";
+                text_input[i]+="| ";
+            }
+            else if(wall[i][j][0] && wall[i][j][3]){
+                cout<<"|_";
+                text_input[i]+="|_";
+            }
+            else if(wall[i][j][3] || wall[i][j][1]){
+                cout<<"__";
+                text_input[i]+="__";
+            }
+            else{
+                cout<<"  ";
+                text_input[i]+="  ";
+            }
         }
         cout<<'\n';
     }
@@ -91,5 +119,10 @@ int main(){
     clear();
     create_maze(2,2);
     draw();
-    cout<<"start(1,1) / end(64,64)\n";
+    string file_name = "",_time = __TIMESTAMP__;
+    cout<<_time<<'\n';
+    ofstream fout;
+    fout.open("/Users/erolf0123/Desktop/c++/Maze/Maze/Maze_data/"+_time);
+    for(int i=64; i>=1; i--) fout<<text_input[i]<<'\n';
+    fout.close();
 }
